b777dc5f14dc773c9d571afab3c24dee902e60f4
Dima Khomko <khomko.dima@gmail.com>
2019-07-29 22:58:43 +0300

    .

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css';
+
+export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
+  render() {
+    return (
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input tupe="text" 
+                className="form-control" 
+                onChange={this.onLabelChange}
+                placeholder="What need to be done" />
+        <button
+          className="btn btn-outline-secondary">
+          Add Item
+        </button>
+      </form>
+    )
+  }
+}
